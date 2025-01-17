7bdcf331810dc30e4bec882928e1d5dec76fb179
Joseph Umana <joseph@josephumana.com>
2025-01-16 23:12:19 -0500

    Finish virtio blk driver init
    Finished the full init process of the virtio blk driver. Calling
    `init_block` makes sure it's a block device and follows the fully
    documented process. Also upgraded to version 2 of the virtio mmio
    communication.
    
    Signed-off-by: Joseph Umana <joseph@josephumana.com>

--- kernel/block.c
+++ kernel/block.c
@@ -1,5 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Author: Joseph Umana
+// Date: 2025-01-16
+// 
+// A simple virtio block device driver.
+// See https://docs.oasis-open.org/virtio/virtio/v1.1/virtio-v1.1.pdf#8f
+
 #include <stdint.h>
-#include <stdlib.h>
+#include <stdbool.h>
+
 #include "panic.h"
 #include "print.h"
 
@@ -8,43 +16,106 @@
 #define BLOCK_REG(x) ((volatile uint32_t *)(BLOCK_ADDRESS + (x)))
 
 #define VIRTIO_MAGIC 0x74726976
-#define VIRTIO_VERSION 0x01
+#define VIRTIO_VERSION 0x02
 
 #define VIRTIO_MAGIC_OFFSET 0x00
 #define VIRTIO_VERSION_OFFSET 0x04
 #define VIRTIO_DEVICE_ID_OFFSET 0x08
 
+#define VIRTIO_STATUS_OFFSET 0x70
+#define VIRTIO_FEATURE_DEVICE_FEATURES_OFFSET 0x10
+#define VIRTIO_DRIVER_FEATURES_OFFSET 0x20
+
+#define RESET_VIRTIO() *BLOCK_REG(VIRTIO_STATUS_OFFSET) = 0
+
+// Status bits
+#define VIRTIO_STATUS_ACKNOWLEDGE 1
+#define VIRTIO_STATUS_DRIVER 2
+#define VIRTIO_STATUS_DRIVER_OK 4
+#define VIRTIO_STATUS_FEATURES_OK 8
+
+struct virtio_blk_device {
+    
+};
+
+__attribute__((warning("Bad feature negotiation. Pls fix ASAP.")))
+void negotiate_features() {
+    volatile uint32_t * reg;
+    reg = BLOCK_REG(VIRTIO_FEATURE_DEVICE_FEATURES_OFFSET);
+    
+    // Okay. This is probably not good. This should *Really* be changed
+    // for a regular thing
+    // But i want this to work for now.
+    // TODO: Also handle read only
+    *BLOCK_REG(VIRTIO_DRIVER_FEATURES_OFFSET) = *reg;
+}
 
 void init_block() {
     volatile uint32_t * reg;
     reg = BLOCK_REG(VIRTIO_MAGIC_OFFSET);
 
     if (*reg != VIRTIO_MAGIC) {
-        panicf("Magic value not found\n");
+        panicf("Magic value not found");
         return;
     }
 
-    printk("virtio: Magic value found\n");
+    printk("virtio: Magic value found");
 
     reg = BLOCK_REG(VIRTIO_VERSION_OFFSET);
     
-    // TODO: We should probably implement version 2
-    // at some point.
     if (*reg != VIRTIO_VERSION) {
         panicf("Version mismatch");
         return;
     }
 
-    printk("virtio: Version value\n");
+    printk("virtio: Version OK");
 
     reg = BLOCK_REG(VIRTIO_DEVICE_ID_OFFSET);
+    
+    // Blank device id should be ignored
+    if (*reg == 0x00)
+        return;
 
-    if (*reg == 0x0) {
-        panicf("Device ID is 0x0\n");
+    printk("virtio: Starting block init");
+
+    // According to docs, we must reset by sending a 0
+    // to the status register.
+    printk("virtio: Resetting device");
+    RESET_VIRTIO();
+
+    // Now we set the ACKNOWLEDGE status bit
+    printk("virtio: ACK device");
+    *BLOCK_REG(VIRTIO_STATUS_OFFSET) |= VIRTIO_STATUS_ACKNOWLEDGE;
+
+    // Check if block device
+    if (*reg != 0x02) {
+        printk("virtio: Invalid device id");
         return;
     }
 
-    printk("virtio: Correct device ID\n");
+    printk("virtio: BLK device DRIVER OK");
 
-    printk("virtio: Initialization complete\n");
+    // Set the DRIVER status bit
+    *BLOCK_REG(VIRTIO_STATUS_OFFSET) |= VIRTIO_STATUS_DRIVER;
+
+    printk("virtio: Negotiating features");
+
+    // Feature negotiation
+    negotiate_features();
+
+    *BLOCK_REG(VIRTIO_DRIVER_FEATURES_OFFSET) |= VIRTIO_STATUS_FEATURES_OK;
+
+    // Check if still OK
+    bool OK = *BLOCK_REG(VIRTIO_STATUS_OFFSET) & VIRTIO_STATUS_FEATURES_OK;
+
+    if (OK)
+        panicf("virtio: Feature subset not supported");
+
+    printk("virtio: Features OK");
+
+    // Driver OK
+    // Yay!
+    *BLOCK_REG(VIRTIO_STATUS_OFFSET) |= VIRTIO_STATUS_DRIVER_OK;
+
+    printk("virtio: Driver OK");
 }
