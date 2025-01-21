3c23a64bca9b7c1b55a2fa8a1e691b74b724870c
Joseph Umana <joseph@josephumana.com>
2025-01-20 20:19:07 -0500

    Added notice
    Added notice about license and copyright information when booting up
    before and after init.
    
    Signed-off-by: Joseph Umana <joseph@josephumana.com>

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -5,10 +5,22 @@
 #include "block.h"
 #include "print.h"
 
+// Printed twice
+// Once before init and once after
+void print_notice() {
+    printk("(C) 2025 Joseph Umana et al.");
+    printk("This software is licensed under the GNU General Public License v2.0 or later.");
+    printk("Some exceptions apply. See COPYING for full details.");
+    printk("This software comes with ABSOLUTELY NO WARRANTY.");
+}
+
 void kmain(void) {
+    print_notice();
+
     init_block();
 
-	printk("Hello world!");
+    print_notice();
+    printk("Hello world!");
 	while(1) {
         uart_debug();
 	}
